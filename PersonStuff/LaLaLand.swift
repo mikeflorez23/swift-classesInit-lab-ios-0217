715e3de4e596d72c4c65621fcbbf8aa5be44567e
Michael Florez <mike_florez@ymail.com>
2017-02-05 23:01:03 -0500

    done

--- PersonStuff/LaLaLand.swift
+++ PersonStuff/LaLaLand.swift
@@ -10,9 +10,15 @@ import Foundation
 
 class LaLaLand {
     
-
+    var sebastian: Person = Person(firstName: "Sebastian", lastName: "Wilder")
+    var mia: Person = Person(firstName: "Mia", lastName: "Dolan")
     
+    func initialMeeting() -> String {
+        return mia.greet(person: sebastian)
+    }
     
-    
-    
+    func dateNight() -> String {
+        return mia.dance(with: sebastian)
+    }
+        
 }
